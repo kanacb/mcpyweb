d1f735cb6a2c0697fd8dc27f8d0ff24eaf63cdea
kanacb <appiahshut@gmail.com>
2021-08-15 23:04:24 +0800

    commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ComponentsModule } from './components/components.module';
+import { RegisterModule } from './register/register.module';
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { ComponentsModule } from './components/components.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ComponentsModule
+    ComponentsModule,
+    RegisterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,11 +8,14 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
-  form: FormGroup = new FormGroup({
-    name: new FormControl('',[Validators.required, Validators.minLength(5), Validators.maxLength(100)]),
-    email: new FormControl('',[Validators.required, Validators.minLength(5), Validators.maxLength(100), Validators.email]),
-  })
-  constructor() { }
+  form: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup({
+      name: new FormControl('',[Validators.required, Validators.minLength(5), Validators.maxLength(100)]),
+      email: new FormControl('',[Validators.required, Validators.minLength(5), Validators.maxLength(100), Validators.email]),
+    })
+   }
 
   ngOnInit(): void {
 
